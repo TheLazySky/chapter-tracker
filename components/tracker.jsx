03b80b59d634dff69d0bd7141eb9eff5b68b7f18
Grant <Grantghx@gmail.com>
2022-06-29 22:50:09 -0400

    Initial Commit

---
+++ components/tracker.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function tracker(){
+    return(
+        <div className='trackerBox'>
+            
+
+        </div>
+    );
+
+
+}
+
+export default tracker;
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.scss'
+import '../components/tracker'
+
+function App() {
+
+  return (
+  <div>
+    <body className='body'></body>
+      <h1 className='header'>Chapter Tracker</h1>
+      <div>
+        <tracker>
+        
+        </tracker>
+      </div>
+  </div>
+
+    )
+}
+
+export default App
